84dd430ac10d87794e400501fe9c8d9cd204375e
Jean-Loïc <jeanloicmugnier@hotmail.com>
2016-07-23 02:35:57 -0300

    working

--- Cluster.py
+++ Cluster.py
@@ -5,13 +5,17 @@ class Cluster:
     '''
     TEsted and working
     '''
-    list_pixel = []
-    avg = np.array([0, 0, 0, 0, 0])
+    list_pixel = np.zeros(5)
+    avg = np.array([0, 0, 0])
+    # PIXEL_SIZE = 5
+    COLOR_SIZE = 3
     PIXEL_SIZE = 5
+    merged = False
 
-    def __init__(self, pixel):
-        self.list_pixel = np.array([pixel])
-        self.avg = pixel
+    def __init__(self, np_pixel):
+        # print("pixel ", pixel)
+        self.list_pixel = np.array([np_pixel])
+        self.avg = np_pixel[2:]
 
     def add_pixel(self, pixel):
         '''
@@ -19,7 +23,7 @@ class Cluster:
         :param pixel: pixel to add
         :return: 0
         '''
-        self.avg_with(pixel)
+        self.avg_with(pixel[2:])
         self.list_pixel = np.append(self.list_pixel, pixel)
         y = self.list_pixel.size / self.PIXEL_SIZE
         self.list_pixel = np.reshape(self.list_pixel, (y, self.PIXEL_SIZE))
@@ -32,12 +36,12 @@ class Cluster:
         :return: 0
         '''
         avg = self.avg
-        nb = self.list_pixel.size / self.PIXEL_SIZE
-        new_avg = np.zeros(self.PIXEL_SIZE)
-        for i in range(self.PIXEL_SIZE):
-            print(i)
-            print(avg)
-            print(pixel)
+        nb = self.list_pixel.size / self.COLOR_SIZE
+        new_avg = np.zeros(self.COLOR_SIZE)
+        for i in range(self.COLOR_SIZE):
+            # print(i)
+            # print(avg)
+            # print(pixel)
             num = nb * avg[i] + pixel[i]
             new_avg[i] = num / (nb + 1)
         self.avg = new_avg
@@ -56,8 +60,10 @@ class Cluster:
         if (self.list_pixel.size != other.list_pixel.size):
             return False
         else:
-            for pixel_ind in range(self.list_pixel.size):
-                if (np.array_equal(self.list_pixel[pixel_ind], other.list_pixel[pixel_ind])):
+            for pixel_ind in range(int(self.list_pixel.size / self.PIXEL_SIZE)):
+                # print("this", self.list_pixel)
+                # print("other", other.list_pixel)
+                if (not (np.array_equal(self.list_pixel[pixel_ind], other.list_pixel[pixel_ind]))):
                     return False
         return True
 
@@ -72,6 +78,9 @@ class Cluster:
         '''
         for self_pixel in self.list_pixel:
             for other_pixel in other.list_pixel:
+                # print(self.list_pixel)
+                # print(other.list_pixel)
+                # print(other_pixel)
                 self_x = self_pixel[0]
                 self_y = self_pixel[1]
                 other_x = other_pixel[0]
@@ -94,6 +103,7 @@ class Cluster:
         '''
         for pixel in cluster.list_pixel:
             self.add_pixel(pixel)
+        cluster.merged = True
         return self
 
     def get_avg(self):
@@ -105,7 +115,7 @@ class Cluster:
     def can_merge(self, other, hr):
         # if (np.linalg.norm(self.avg[2:] - other.avg[2:], 2) < hr):
         if (np.linalg.norm(self.avg - other.avg, 2) < hr):
-            print("CAN MERGE")
+            # print("CAN MERGE")
             return True
         return False
 
@@ -115,10 +125,19 @@ class Cluster:
     def get_pixels(self):
         return self.list_pixel
 
+#
 # pixel = np.array([0, 0, 0, 0, 0])
+# pixel3 = np.array([0, 1, 0, 0, 0])
+# pixel4 = np.array([1, 0, 0, 0, 0])
+# clt3 = Cluster(pixel3)
 # clt = Cluster(pixel)
 # clt.add_pixel(np.array([1, 1, 1, 1, 1]))
 # pixel2 = np.array([255, 255, 255, 255, 255])
 # clt2 = Cluster(pixel2)
 # clt2.add_pixel(np.array([244, 244, 244, 244, 244]))
 # clt.add_cluster(clt2)
+#
+# print(clt.list_pixel)
+# print(clt.avg)
+# print(clt2.list_pixel)
+# print(clt3.is_neighbors(clt))
--- Segmentation.py
+++ Segmentation.py
@@ -14,50 +14,42 @@ class Segmentation:
 
     def __init__(self, img):
         clusters = np.array(img)
-        # print("clusters")
-        # print(clusters)
-        # print("clusters size")
-        # print(clusters.size)
         self.img_size = img.size
-        # print("self.img_size")
-        # print(self.img_size)
         pixel_size = clusters[0][0].size
         y_max = int(clusters[0].size / pixel_size)
         x_max = int(clusters.size / y_max / pixel_size)
-        # print("larg and width")
-        # print(x_max)
-        # print(y_max)
-        # self.list_cluster = np.zeros((1, x_max * y_max, pixel_size), dtype=Cluster.Cluster)
-        # print(self.list_cluster[0])
-        # print(self.list_cluster[0][0])
         for x in range(x_max):
             for y in range(y_max):
-                # print("x,y", x, y)
-                # print("self.list_cluster[x][y]", self.list_cluster[x][y])
-                # print("clusters[x][y]", clusters[x][y])
-                self.list_cluster = np.append(self.list_cluster, Cluster.Cluster(clusters[x][y]))
+                xy = np.array([x, y])
+                xycolor = np.append(xy, clusters[x][y])
+                self.list_cluster = np.append(self.list_cluster, Cluster.Cluster(xycolor))
         self.list_cluster = np.delete(self.list_cluster, 0)
 
-    def run_segmentation(self, hr):
-        for fir_clust_ind in range(len(self.list_cluster)):
-            for sec_clust_ind in range(len(self.list_cluster)):
-                if (not (self.list_cluster[fir_clust_ind].equals(self.list_cluster[sec_clust_ind]))):
-                    # the two clusters aren't the same
-                    if (self.list_cluster[fir_clust_ind].is_neighbors(self.list_cluster[sec_clust_ind])):
-                        # the clusters are neinhors in 2D
-                        if (self.list_cluster[fir_clust_ind].can_merge(self.list_cluster[sec_clust_ind], hr)):
-                            # the clusters can merge RGB proximity
-                            # add the sec cluter to the irst
-                            self.list_cluster[fir_clust_ind].add_cluster(self.list_cluster[sec_clust_ind])
-                            # remove the second cluster (merged one) from the list of clusters
-                            np.delete(self.list_cluster[sec_clust_ind])
+    def run_segmentation(self, hr, nb_iter):
+        new = np.array([])
+        for iter in range(nb_iter):
+            for fir_clust_ind in range(len(self.list_cluster)):
+                print(fir_clust_ind)
+                for sec_clust_ind in range(len(self.list_cluster)):
+                    # print(self.list_cluster[fir_clust_ind].avg)
+                    # print(self.list_cluster[fir_clust_ind].list_pixel)
+                    if (not (self.list_cluster[sec_clust_ind].merged)):
+                        if (not (self.list_cluster[fir_clust_ind].equals(self.list_cluster[sec_clust_ind]))):
+                            if (self.list_cluster[fir_clust_ind].is_neighbors(self.list_cluster[sec_clust_ind])):
+                                if (self.list_cluster[fir_clust_ind].can_merge(self.list_cluster[sec_clust_ind], hr)):
+                                    self.list_cluster[fir_clust_ind].add_cluster(self.list_cluster[sec_clust_ind])
+                                    # self.list_cluster = np.delete(self.list_cluster,sec_clust_ind)
 
     def create_segmented_image(self):
         img = Image.new("RGB", self.img_size)
         for clust in self.list_cluster:
-            for pixel in clust.get_pixel():
+            for pixel in clust.list_pixel:
                 pixel[2:] = clust.get_avg()
-                img.putpixel((pixel[0], pixel[1]), pixel[2:])
+                #        pupixel needs xy, but cluser doesnt have the pixel postion
+                # print(clust.avg)
+                # print(np.round(clust.avg))
+                # print((pixel[1], pixel[0]), tuple(np.round(clust.avg)))
+                img.putpixel((pixel[1], pixel[0]), tuple(np.round(clust.avg).astype(int)))
         return img
 
     def get_list_cluster(self):
@@ -68,13 +60,13 @@ class Segmentation:
             print("self.list_cluster SIZE", self.list_cluster.size)
             print("clust", clust)
             print("size", clust.get_size())
-            print("avg", clust.get_avg())
+            print("avg", clust6.get_avg())
             print("pizels", clust.get_pixels())
 
 
 # epsi =.1
 # neighbors =50
-hr=10
+hr = 50
 # hs=10
 #
 img = Image.open("images/fruit_filtered")
@@ -82,6 +74,6 @@ img = Image.open("images/fruit_filtered")
 # new = msf.run_mean_shift()
 # new.show()
 seg = Segmentation(img)
-seg.run_segmentation(hr)
+seg.run_segmentation(hr, 6)
 seg.get_clusters_det()
 seg.create_segmented_image().show()
